3cc1e91034d96584be2f881045f021b234e29c74
xtrem3500 <xtrem3500@gmail.com>
2025-06-16 17:09:14 +0000

    vercel 07

---
+++ env.d.ts
@@ -0,0 +1,5 @@
+// env.d.ts
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_ANON_KEY: string
+}
---
+++ src/lib/supabase/supabase.js
@@ -0,0 +1,5 @@
+// src/lib/supabase.js
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
